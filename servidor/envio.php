2703c96226c52dbd863f2eb5e20211c23b54f525
jorge.agustin.deaquino@gmail.com <jorge.agustin.deaquino@gmail.com>
2022-06-17 18:25:21 -0500

    se agrego boton registrar en el login y la opción 'olvido su contraseña' con su respectivo formulario para recuperar la contraseña.

--- servidor/envio.php
+++ servidor/envio.php
@@ -1,31 +1,3 @@
 <?php
-//     echo "mensaje de prueba";
 
-// |   $servername = "127.0.0.1";
-// |   $database = "spa_dental_lindavista";
-// |   $username = "root@localhost";
-// |   $password = "";
-// // Create connection
-// $conn = mysqli_connect($servername, $username, $password, $database);
-// // Check connection
-// if (!$conn) {
-//     die("Connection failed:" . mysqli_connect_error());
-// }
-// echo "Connected successfully";
-// mysqli_close($conn);
-?>
-
-<?php
-$servername = "localhost";
-$database = "spa_dental_lindavista";
-$username = "root";
-$password = "";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-mysqli_close($conn);
 ?>
