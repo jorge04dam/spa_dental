0ffc6835232f2cc21d608e5150b6d77ed6d6260f
jorge dam <jorge.agustin.deaquino@gmail.com>
2022-06-20 17:52:39 -0600

    cambios en el tablero de pendientes.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
-let iniciarSesion 			= document.getElementById("btn_ini");
-let btn_cancelar 			= document.getElementById("btn_cancel");
-let btn_creando				= document.getElementById("btn_creando");
-let nombre 					= document.getElementById("name");
-let usuario 				= document.getElementById("name_user");
-let contraseña 				= document.getElementById("password");
-let confirmar_contraseña	= document.getElementById("confirm_password");
-let add 					= document.getElementById("add");
-let cerrar_sesion			= document.getElementById("usuario_sesion");
-let registrar				= document.getElementById("registrar");
-let recuperar				= document.getElementById("recuperar");
+const iniciarSesion 		= document.getElementById("btn_ini");
+const btn_cancelar 			= document.getElementById("btn_cancel");
+const btn_creando			= document.getElementById("btn_creando");
+const nombre 				= document.getElementById("name");
+const usuario 				= document.getElementById("name_user");
+const contraseña 			= document.getElementById("password");
+const confirmar_contraseña	= document.getElementById("confirm_password");
+const add 					= document.getElementById("add");
+const cerrar_sesion			= document.getElementById("usuario_sesion");
+const registrar				= document.getElementById("registrar");
+const recuperar				= document.getElementById("recuperar");
+const estilosPendiente		= document.getElementById("cardPendiente");
+const estilosenProceso		= document.getElementById("cardenProceso");
+const estilosRealizado		= document.getElementById("cardRealizado");
+const estilos 				= document.getElementById("cardTareas");
 
 let validarUsuario = (usuario, contraseña) => {
 	if(usuario.value == credencialesUsuario.usuario && 
@@ -83,11 +87,22 @@ let baseUsuarios = {
 	contraseña: ""
 }
 
-let inicioSesion = () =>{
-	if (1===1){
-		// window.location.replace("login.html");
-		return true;
-	}
+// let inicioSesion = () =>{
+// 	if (1===1){
+// 		// window.location.replace("login.html");
+// 		return true;
+// 	}
+// }
+
+// console.log(inicioSesion());
+
+// definir el estado de cada tarea en el tablero de pendientes
+let estado = {
+	pendiente: true,
+	enProceso: false,
+	realizado: false
 }
 
-console.log(inicioSesion());
+if(estado.pendiente == true){
+	cardRealizado
+}
