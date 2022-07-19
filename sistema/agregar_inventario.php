5381afbd62c5dfbc3926ac6dde315220b097f093
jorge dam <jorge.agustin.deaquino@gmail.com>
2022-07-19 18:48:04 -0500

    formulario agregar inventario

--- sistema/agregar_inventario.php
+++ sistema/agregar_inventario.php
@@ -1,6 +1,10 @@
 <link rel="stylesheet" href="../pruebas/css/inventory_add.css">
 <form action="" id="inventory_add">
-    <input type="text" placeholder="producto">
+    <select name="" id="" placeholder="marca">
+        <option value="0">producto</option>
+        <option value="1">producto1</option>
+        <option value="2">producto2</option>
+    </select>
     <select name="" id="" placeholder="marca">
         <option value="0">marca</option>
         <option value="1">kirlklan</option>
@@ -14,9 +18,26 @@
     
     <input type="date" placeholder="fecha">
     <input type="text" placeholder="cantidad en pzs">
-    <input type="text" placeholder="precio unitario">
     <input type="text" placeholder="precio total">
-    <input type="text" placeholder="tipo de insumo">
-    <input type="text" placeholder="insumo">
-    <input type="text" placeholder="forma de pago">
+    <input type="text" placeholder="precio unitario">
+    <select name="" id="" >
+        <option value="0">Tipo insumo</option>
+        <option value="1">Consumible</option>
+        <option value="2">instrumental</option>
+    </select>
+    
+    <select name="" id="" >
+        <option value="0">Insumo</option>
+        <option value="1">Dental</option>
+        <option value="2">Papeleria</option>
+        <option value="3">Limpieza</option>
+    </select>
+    <select name="" id="" >
+        <option value="0">Forma de pago</option>
+        <option value="1">Efectivo</option>
+        <option value="2">Tarjeta de débito</option>
+        <option value="3">Tarjeta de crédito</option>
+        <option value="3">Transferencia</option>
+    </select>
+    <input type="button" value="Guardar insumo" id="btn_guardar_insumo">
 </form>
