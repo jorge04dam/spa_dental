5502919bba3313f38138c2dd13b168916b8bc6d4
jorge dam <jorge.agustin.deaquino@gmail.com>
2022-07-22 18:26:47 -0500

    inventario

--- sistema/agregar_inventario.php
+++ sistema/agregar_inventario.php
@@ -51,6 +51,23 @@
     <br>
     <br><br>
         <form action="../servidor/agregar_insumo.php" method="post" id="inventory_add">
+
+        <select name="" id="insumo" >
+            <?php
+                while($fila = mysqli_fetch_array($datos_input, MYSQLI_ASSOC)){
+                    //echo "<option value=".$fila['id_product'].">".$fila['name_product']."</option>";
+                    echo "<option value=".$fila['id_input'].">".$fila['name_input']."</option>";
+                }
+            ?>
+        </select>
+        <select name="" id="tipo_insumo" >
+            <?php
+            while($fila = mysqli_fetch_array($datos_type_of_input, MYSQLI_ASSOC)){
+                //echo "<option value=".$fila['id_product'].">".$fila['name_product']."</option>";
+                echo "<option value=".$fila['id_type_of_input'].">".$fila['type_of_input']."</option>";
+            }
+            ?>
+        </select>
             <select name="" id="product" placeholder="marca">
             <?php
                 while($fila = mysqli_fetch_array($datos_producto, MYSQLI_ASSOC)){
@@ -68,16 +85,19 @@
             }
         ?>
         </select>
+        <input type="text" id= "piezas" placeholder="cantidad en pzs">
+        <input type="text" id= "precio_total" placeholder="precio total">
+        <p id="precio_unitario" onclick = "unitario()">precio unitario</p>
         <select name="" id="brend" placeholder="marca">
-        <?php
+            <?php
             while($fila = mysqli_fetch_array($datos_brend, MYSQLI_ASSOC)){
                 //echo "<option value=".$fila['id_product'].">".$fila['name_product']."</option>";
                 echo "<option value=".$fila['id_brend'].">".$fila['name_brend']."</option>";
             }
-        ?>
-    </select>
-    <select name="" id="provider" >
-    <?php
+            ?>
+        </select>
+        <select name="" id="provider" >
+            <?php
             while($fila = mysqli_fetch_array($datos_provider, MYSQLI_ASSOC)){
                 //echo "<option value=".$fila['id_product'].">".$fila['name_product']."</option>";
                 echo "<option value=".$fila['id_provider'].">".$fila['name_provider']."</option>";
@@ -86,27 +106,7 @@
     </select>
     
     <input type="date" id="fecha" placeholder="fecha">
-    <input type="text" id= "piezas" placeholder="cantidad en pzs">
-    <input type="text" id= "precio_total" placeholder="precio total">
-    <p id="precio_unitario" onclick = "unitario()">precio unitario</p>
-    
-    <select name="" id="tipo_insumo" >
-    <?php
-            while($fila = mysqli_fetch_array($datos_type_of_input, MYSQLI_ASSOC)){
-                //echo "<option value=".$fila['id_product'].">".$fila['name_product']."</option>";
-                echo "<option value=".$fila['id_type_of_input'].">".$fila['type_of_input']."</option>";
-            }
-        ?>
-    </select>
-    
-    <select name="" id="insumo" >
-    <?php
-            while($fila = mysqli_fetch_array($datos_input, MYSQLI_ASSOC)){
-                //echo "<option value=".$fila['id_product'].">".$fila['name_product']."</option>";
-                echo "<option value=".$fila['id_input'].">".$fila['name_input']."</option>";
-            }
-        ?>
-    </select>
+
     <select name="" id="forma_pago" >
     <?php
             while($fila = mysqli_fetch_array($datos_way_to_pay, MYSQLI_ASSOC)){
@@ -140,3 +140,18 @@
 
 
 ?>
+
+<!-- orden para agregar inventario
+de lo general a lo particular
+1  Area             : dental/limpieza/oficina
+2  Tipo de insumo   : consumible/instrumental
+3  Producto         : cubreboca/guante
+4  Tipo unidad      : mililitro/litro/miligramo/gramo
+5  Cantidad         : numero de unidades
+6  Precio total     : precio total de lo que se adquirio
+7  Precio unitario  : precio de una unidad
+8  Marca            : marca del producto
+9  Fecha            : fecha en la que se hace el registro
+10 Forma de pago    : efectivo/transferencia/entre otros.
+11 Proveedor        : Nombre del lugar donde se adquieren -->
+
